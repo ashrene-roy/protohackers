eba32cc6e2499910cd724df827430a1dc4294ef8
AshreneRoy <ashreneroy@gmail.com>
2022-11-17 13:15:55 +0530

    feat: json request response server for prime time

---
+++ 1.prime-time/index.js
@@ -0,0 +1,60 @@
+const net = require('net');
+const server = net.createServer();
+
+const isPrime = (num) => {
+    const squareRoot = Math.sqrt(num);
+    if(num > 3 && (num % 2 == 0 || num % 3 == 0)) return false;
+    for(let i = 2; i <= squareRoot; i++) {
+        if(num % i == 0) return false;
+    }
+    return num >= 2 && Number.isInteger(num);
+}
+const handleConnection = (connection) => {
+    const address = JSON.stringify(connection.remoteAddress + ':' + connection.remotePort);
+    console.log('New remote connection from: ' + address);
+
+    let buffer = '';
+    connection.on('data', (data) => {
+        try {
+            
+            let prev = 0, next;
+            data = data.toString();
+            while((next = data.indexOf('\n', prev)) > -1) {
+                buffer += data.substring(prev, next);
+                console.log('Data from: ' + address + ' ' + buffer);
+                const parsedJSON = JSON.parse(buffer);
+                
+                if(parsedJSON.method == null || parsedJSON.method != 'isPrime') {
+                    throw new Error("Malformed request!");
+                }
+                if(parsedJSON.number == null || typeof(parsedJSON.number) !== 'number') {
+                    throw new Error("Malformed request!");
+                }
+                let response = {
+                    method: "isPrime",
+                    prime: isPrime(parsedJSON.number)
+                };
+                connection.write(JSON.stringify(response) + '\n');
+                buffer = '';
+                prev = next + 1;
+            }
+            buffer += data.substring(prev);
+        } catch (err) {
+            let errResponse = {
+                method: ""
+            };
+            if(connection.write(JSON.stringify(errResponse) + '\n')) {
+                connection.destroy();
+            }
+        }
+    });
+    connection.on('close', () => {
+        console.log('Connection closed with: ' + address);
+    })
+}
+
+server.on('connection', handleConnection);
+
+server.listen(8000, () => {
+    console.log('Server listening on port 8000');
+})
