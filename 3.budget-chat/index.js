63f8c195bf5232da631be3b1fbf380c42a0f03b3
AshreneRoy <ashreneroy@gmail.com>
2022-11-20 00:03:01 +0530

    enhance: solution for tcp budget chat

--- 3.budget-chat/index.js
+++ 3.budget-chat/index.js
@@ -4,6 +4,19 @@ const crypto = require('crypto');
 const server = net.createServer();
 let users = [];
 
+const validateUserName = (name) => {
+    return name.length > 0 && ((/^[a-zA-Z0-9]+$/).test(name)) && name.length < 17;
+}
+
+const broadcast = (presentUser, message, cb = null) => {
+    users.forEach(user => {
+        if(user.id !== presentUser.id && user.joined) {
+            console.log(message);
+            user.connection.write(message);
+            if(cb) cb(user);
+        }
+    });
+}
 const handleConnection = (connection) => {
     
     connection.write('Welcome to budgetchat! What shall I call you?\n');
@@ -21,56 +34,39 @@ const handleConnection = (connection) => {
             return;
         }
         
-        console.log('--- ' + data + ' ' + connection.remoteAddress+':' +connection.remotePort);
         if(!user.joined) {
             user.name = data;
-            if( user.name.length < 1 ||!((/^[a-zA-Z0-9]+$/).test(user.name)) || user.name.length > 17 ) {
-
+            if(!validateUserName(user.name) ) {
                 let newUsers = users.filter(u => {
                     return u.id !== user.id;
                 });
                 users = newUsers;
-                
                 connection.destroy();
                 return;
             }
             
             let others = '';
-            users.forEach(u => {
-                
-                if(u.id !== user.id && u.joined) {
-                    others += u.name + ', ';
-                    console.log(u.name + ' * ' + user.name + ' has entered the room\n');
-                    u.connection.write('* ' + user.name + ' has entered the room\n');
-                }
+            broadcast(user, '* ' + user.name + ' has entered the room\n', (u) => {
+                others += u.name + ', ';
             });
-            console.log('* The room contains: ' + others.slice(0, others.length-2) + '\n');
+            console.log(others);
             connection.write('* The room contains: ' + others.slice(0, others.length-2) + '\n');
             user.joined = true;
             users.push(user);
         } else {
-            users.forEach(u => {
-                if(u.id !== user.id) {
-                    console.log(u.name + ' [' + user.name + '] ' + data.slice(0, 1001));
-                    u.connection.write('[' + user.name + '] ' + data.slice(0, 1001) + '\n');
-                }
-            });
+            broadcast(user, '[' + user.name + '] ' + data.slice(0, 1001) + '\n');
         }
         data = '';
-        return
+        return;
     });
     connection.on('close', () => {
-        users.forEach(u => {
-            console.log(`uId ${u.id} userId ${user.id} userName ${user.name}`);
-            if(u.id !== user.id && u.joined && user.joined) {
-                console.log(u.name + ' * ' + user.name + ' has left the room\n');
-                u.connection.write('* ' + user.name + ' has left the room\n');
-            }
-        });
-        let newUsers = users.filter(u => {
-            return u.id !== user.id;
-        });
-        users = newUsers;
+        if(users.includes(user))  {
+            broadcast(user, '* ' + user.name + ' has left the room\n');
+            let newUsers = users.filter(u => {
+                return u.id !== user.id;
+            });
+            users = newUsers;
+        }
         console.log('Connection closed with client: ' + user.name);
     });
 }
